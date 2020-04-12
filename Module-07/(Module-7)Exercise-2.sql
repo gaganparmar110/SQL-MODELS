87dcdd0ff95a80689d26d092295c70f3f9099825
Gagan <Gagan@GAGAN-UM74RFOR>
2020-04-12 23:05:04 +0530

    update

--- Module-07/(Module-7)Exercise-2.sql
+++ Module-07/(Module-7)Exercise-2.sql
@@ -2,51 +2,6 @@
 USE [AdventureWorks2017]
 GO
 
-crete tabel
-CREATE TABLE [dbo].[FactInternetSales]
-(
-	[ProductKey] [int] NOT NULL,
-	[OrderDateKey] [int] NOT NULL,
-	[DueDateKey] [int] NOT NULL,
-	[ShipDateKey] [int] NOT NULL,
-	[CustomerKey] [int] NOT NULL,
-	[PromotionKey] [int] NOT NULL,
-	[CurrencyKey] [int] NOT NULL,
-	[SalesTerritoryKey] [int] NOT NULL,
-	[SalesOrderNumber] [nvarchar](20) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
-	[SalesOrderLineNumber] [tinyint] NOT NULL,
-	[RevisionNumber] [tinyint] NOT NULL,
-	[OrderQuantity] [smallint] NOT NULL,
-	[UnitPrice] [money] NOT NULL,
-	[ExtendedAmount] [money] NOT NULL,
-	[UnitPriceDiscountPct] [float](53) NOT NULL,
-	[DiscountAmount] [float](53) NOT NULL,
-	[ProductStandardCost] [money] NOT NULL,
-	[TotalProductCost] [money] NOT NULL,
-	[SalesAmount] [money] NOT NULL,
-	[TaxAmt] [money] NOT NULL,
-	[Freight] [money] NOT NULL,
-	[CarrierTrackingNumber] [nvarchar](25) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-	[CustomerPONumber] [nvarchar](25) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-	[OrderDate] [datetime] NULL,
-	[DueDate] [datetime] NULL,
-	[ShipDate] [datetime] NULL,
-
- CONSTRAINT [FactInternetSales_primaryKey]  PRIMARY KEY NONCLUSTERED HASH 
-(
-	[SalesOrderNumber],
-	[SalesOrderLineNumber]
-)WITH ( BUCKET_COUNT = 2097152)
-,INDEX CCI_OnlineFactInternetSales CLUSTERED COLUMNSTORE
-)WITH ( MEMORY_OPTIMIZED = ON , DURABILITY = SCHEMA_AND_DATA )
-GO
-
-
-
-
-
-
-
 
 CREATE CLUSTERED COLUMNSTORE INDEX CCI_SalesOrderDetail ON Sales.SalesOrderDetail
 GO
