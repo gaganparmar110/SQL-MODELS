7e55d08d66c80306181c058f47fa8a481482bbbe
Gagan <Gagan@GAGAN-UM74RFOR>
2020-04-16 23:05:13 +0530

    changes

--- Module-10/(Module-10)LabExercise-2.sql
+++ Module-10/(Module-10)LabExercise-2.sql
@@ -1,11 +1,32 @@
 Use SqlAssignment;
 Go
 
-create function StringList()
+CREATE FUNCTION dbo.IntegerListToTable
+( @InputList varchar(MAX),@Delimiter char(1) = ',')
+RETURNS @OutputTable TABLE (PositionInList int IDENTITY(1, 1) NOT NULL,IntegerValue int)
+AS BEGIN
+		INSERT INTO @OutputTable (IntegerValue)
+		SELECT Value FROM STRING_SPLIT ( @InputList , @Delimiter );
+	RETURN;
+END;
+GO
+
+
+SELECT * FROM 
+dbo.IntegerListToTable('234,354253,3242,2',',');
+GO
+
+SELECT * FROM 
+dbo.IntegerListToTable('234|354253|3242|2','|');
+GO
+
+
+********
+alter function StringList()
 returns varchar(100)
 as begin
 return (
-select STRING_AGG(EmployeeName,'-') from Employees)
+select STRING_AGG(FirstName,'-') from JoinQuery.Employee)
 end
 Go
 
